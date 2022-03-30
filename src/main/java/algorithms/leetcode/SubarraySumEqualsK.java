0ee9dc9f8d70fff0e747a726c08a4b9d44547c5f
Ricardo Cervo <ricardo.cervo@gft.com>
2022-03-30 14:38:03 -0300

    Subarray sum equals K

---
+++ src/main/java/algorithms/leetcode/SubarraySumEqualsK.java
@@ -0,0 +1,71 @@
+package algorithms.leetcode;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Test;
+
+public class SubarraySumEqualsK {
+
+	/*
+	 * Using HashMap O(n):
+	 */
+
+	public int subarraySum(int[] nums, int k) {
+		HashMap<Integer, Integer> mapSums = new HashMap<>();
+		mapSums.put(0, 1);
+		int sum = 0;
+		int result = 0;
+		for (int i = 0; i < nums.length; i++) {
+			sum += nums[i];
+			if (mapSums.containsKey(sum - k)) {
+				result += mapSums.get(sum - k);
+			}
+			mapSums.put(sum, mapSums.getOrDefault(sum, 0) + 1);
+		}
+		return result;
+	}
+
+	/*
+	 * Brute-force O(n²)
+	 */
+//	public int subarraySum(int[] nums, int k) {
+//
+//		int sum = 0;
+//		int ans = 0;
+//		HashMap<Integer, Integer> map = new HashMap<>();
+//		map.put(0, 1);
+//		
+//		for (int i = 0; i < nums.length; i++) {
+//			sum += nums[i];
+//			if (map.containsKey(sum - k)) {
+//				ans += map.get(sum - k);
+//			}
+//			map.put(sum, map.getOrDefault(sum, 0) + 1);
+//		}
+//		return ans;
+//	}
+
+	@Test
+	public void test0() {
+		int arr[] = { 1, 1, 0 };
+		int k = 2;
+		assertEquals(2, subarraySum(arr, k));
+	}
+
+	@Test
+	public void test1() {
+		int arr[] = { 1, 1, 1, 0, 0 };
+		int k = 2;
+		assertEquals(4, subarraySum(arr, k));
+	}
+
+	@Test
+	public void test2() {
+		int arr[] = { 1, 2, 3 };
+		int k = 3;
+		assertEquals(2, subarraySum(arr, k));
+	}
+}
