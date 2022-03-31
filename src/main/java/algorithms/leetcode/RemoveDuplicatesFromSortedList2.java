ac7e8c77f50ff0eebd6b7198fad2eace1218fcb1
Ricardo Cervo <ricardo.cervo@gft.com>
2022-03-31 18:32:12 -0300

    RemoveDuplicatesFromSortedList2

---
+++ src/main/java/algorithms/leetcode/RemoveDuplicatesFromSortedList2.java
@@ -0,0 +1,95 @@
+package algorithms.leetcode;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.Test;
+
+public class RemoveDuplicatesFromSortedList2 {
+
+	private class ListNode {
+		int val;
+		ListNode next;
+
+		ListNode() {
+		}
+
+		ListNode(int val) {
+			this.val = val;
+		}
+
+		ListNode(int val, ListNode next) {
+			this.val = val;
+			this.next = next;
+		}
+
+		@Override
+		public String toString() {
+			return "ListNode [val=" + val + "]";
+		}
+		
+	}
+	
+	boolean threeDifferent(ListNode prev, ListNode current) {
+		return prev.val != current.val && (current.next == null || current.next.val != current.val); 
+	}
+	
+	public ListNode deleteDuplicates(ListNode head) {
+		if (head == null || head.next == null) {
+			return head;
+		}
+		
+		ListNode dummy = new ListNode(-99999, head);
+
+		ListNode prev = dummy;
+		ListNode current = head;
+		
+		while (true) {
+			
+			while (!threeDifferent(prev, current)) {
+				current = current.next;
+			}
+			
+			prev.next = current;
+			prev = current;
+			current = current.next;
+			if (current == null) {
+				break;
+			}
+			
+		}
+		
+		return dummy.next;
+		
+	}
+
+	@Test
+	public void test1() {
+		ListNode list = new ListNode(1, new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(3, new ListNode(4))))));
+		ListNode noDuplicates = deleteDuplicates(list);
+		String expected = "2, 4";
+		String result = listToString(noDuplicates);
+		assertEquals(expected, result);
+	}
+//
+//	@Test
+//	public void test2() {
+//		ListNode list = new ListNode(1, new ListNode(1, new ListNode(1, new ListNode(3))));
+//		ListNode noDuplicates = deleteDuplicates(list);
+//		String expected = "1, 3";
+//		String result = listToString(noDuplicates);
+//		assertEquals(expected, result);
+//	}
+//
+	public String listToString(ListNode head) {
+		ListNode pointer = head;
+		String str = "";
+		while (pointer != null) {
+			if (!str.isEmpty()) {
+				str += ", ";
+			}
+			str += pointer.val;
+			pointer = pointer.next;
+		}
+		return str;
+	}
+}
