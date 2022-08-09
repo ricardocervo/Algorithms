60e10ed622d879cf98dbd7cef2619c541c758f21
Ricardo Cervo <ricardo.a.cervo@gmail.com>
2022-08-09 02:51:11 -0300

    Backtracking tests

--- src/main/java/algorithms/leetcode/BackTrackingTest.java
+++ src/main/java/algorithms/leetcode/BackTrackingTest.java
@@ -4,33 +4,69 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class BackTrackingTest {
-	void init() {
-		String alphabet = "0123456789";
-		int n = 9;
-
-		char[] answer = new char[n];
-		List<String> answers = new ArrayList<>();
-		backtrack(0, answers, answer, alphabet);
-		for (String s : answers) {
-			System.out.println(s);
-		}
+	void initTests() {
+		String alphabet = "abc";
+		int n = 4;
+		List<String> result = new ArrayList<>();
+		bruteForce(alphabet, result, 0, new char[n]);
+		
+		System.out.println(result);
+		
+		result.clear();
+		permutation(alphabet, result, 0, alphabet.toCharArray());
+		System.out.println(result);
+		
+		result.clear();
+		subset(alphabet, result, 0, "");
+		System.out.println(result);
 	}
-
-	void backtrack(int depth, List<String> answers, char[] answer, String alphabet) {
+	
+	void bruteForce(String alphabet, List<String> result, int depth, char[] answer) {
 		if (depth == answer.length) {
-			answers.add(new String(answer));
+			result.add(new String(answer));
 			return;
 		}
-
-		for (int i = 0; i < alphabet.length(); i++) {
-			char c = alphabet.charAt(i);
-			answer[depth] = c;
-			backtrack(depth + 1, answers, answer, alphabet);
+		
+		for (int i=0; i<alphabet.length(); i++) {
+			answer[depth] = alphabet.charAt(i);
+			bruteForce(alphabet, result, depth + 1, answer);
 		}
-
 	}
-
+	
+	void permutation(String alphabet, List<String> result, int depth, char[] answer) {
+		if (depth == answer.length) {
+			result.add(new String(answer));
+			return;
+		}
+		
+		for (int i=depth; i<answer.length; i++) {
+			swap(answer, depth, i);
+			permutation(alphabet, result, depth+1, answer);
+			swap(answer, depth, i);
+		}
+	}
+	
+	void subset(String alphabet, List<String> result, int depth, String answer) {
+		if (depth == alphabet.length()) {
+			result.add(answer);
+			return;
+		}
+		
+		subset(alphabet, result, depth+1, answer);
+		subset(alphabet, result, depth+1, answer + alphabet.charAt(depth));
+	}
+	
+	void swap(char[] array, int i, int j) {
+		char tmp = array[i];
+		array[i] = array[j];
+		array[j] = tmp;
+	}
+	
+	void subset(String alphabet, List<String> result, int depth) {
+		
+	}
+	
 	public static void main(String[] args) {
-		new BackTrackingTest().init();
+		new BackTrackingTest().initTests();
 	}
 }
